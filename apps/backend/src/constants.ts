64d9c362e0dc82da849995cf15b44ade0937f20e
benbaessler <mail@benbaessler.com>
2023-11-24 12:20:44 +0000

    feat(backend): default profile picture

--- apps/backend/src/constants.ts
+++ apps/backend/src/constants.ts
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: '../../.env' });
+require("dotenv").config({ path: "../../.env" });
 
 export const useMainnet: boolean = process.env.USE_MAINNET == "true";
 
@@ -11,8 +11,8 @@ export const SENTRY_DSN = process.env.SENTRY_DSN!;
 
 export const interactionProxyAddress = useMainnet
   ? "0xcbea63064afbfab509c33f9843fd8e08336d5971"
-  // TODO: Add testnet interaction proxy address
-  : "";
+  : // TODO: Add testnet interaction proxy address
+    "";
 
 export const topics = {
   post: "0xe18912378f90aa372fc9ab7ab5ff7e4744182bdef133ccad56d5a18864456742",
@@ -23,7 +23,7 @@ export const topics = {
   transfer:
     "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef",
   // not used currently
-  follow: "0x817d2c71a3ec35dc50f2e4b0d890943c89f2a7ab9d96eff233eda4932b506d0b"
+  follow: "0x817d2c71a3ec35dc50f2e4b0d890943c89f2a7ab9d96eff233eda4932b506d0b",
 };
 
 export const graphEndpoint = useMainnet
@@ -31,18 +31,19 @@ export const graphEndpoint = useMainnet
   : "wss://api-v2-mumbai.lens.dev";
 
 export const appIcons: { [key: string]: string } = {
-  hey:
-    "https://hey.xyz/logo.png",
+  hey: "https://hey.xyz/logo.png",
   orb: "https://assets-global.website-files.com/6364e65656ab107e465325d2/637adce0f72214c270ffa76e_JQtTVb7vwFnUVbc9IshTcAUewaoeO8Up_B6pqkeRFGA.jpeg",
   phaver:
     "https://play-lh.googleusercontent.com/zvGNd9l1io5wL0SVxzqIrMzA0bRoO8Dsxmam8rj_KYZSi7hxg8L7VS-_HO2HxM3k9ak",
-  tape:
-    "https://static.tape.xyz/brand/logo.svg",
+  tape: "https://static.tape.xyz/brand/logo.svg",
   buttrfly: "https://buttrfly.app/buttrfly-icon-rounded.png",
   chainjet:
     "https://pbs.twimg.com/profile_images/1567155757761679361/k_EIJBD5_400x400.jpg",
   unknown: "https://i.imgur.com/GGXvzmz.png",
 };
+
+export const defaultProfilePicture =
+  "https://raw.githubusercontent.com/lens-protocol/brand-kit/074e865b5da4b2b80133915b15e82f9ba1f02881/01%20Logo/SVG/Icon-Green.svg";
 
 export const dbConnectionString = useMainnet
   ? process.env.DB_CONN_STRING!
