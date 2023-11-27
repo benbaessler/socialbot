24087729ab7329d74d1546e883345581e117f39d
benbaessler <mail@benbaessler.com>
2023-11-27 16:44:36 +0000

    feat(backend): handle v2 momoka transactions

--- apps/backend/src/handlers/dataAvailability.ts
+++ apps/backend/src/handlers/dataAvailability.ts
@@ -1,7 +1,7 @@
 import { WebSocket } from "ws";
 import { createClient } from "graphql-ws";
 import {
-  getMonitoredHandles,
+  getMonitoredProfileIds,
   MessageContent,
   PublicationEmbed,
   getPublicationUrl,
@@ -9,17 +9,11 @@ import {
   getPublicationById,
   hexToNumber,
 } from "../utils";
-import { graphEndpoint, userAgent } from "../constants";
-import { newTransactionQuery } from "../graphql/NewTransactionSubscription";
-import { MomokaTransaction } from "../generated";
 import { captureException } from "@sentry/node";
+import { graphEndpoint } from "../constants";
+import { IMomokaTransaction } from "../types";
+import { newTransactionQuery } from "../graphql/NewTransactionSubscription";
 import { sendToDiscord } from ".";
-import {
-  CommentFragment,
-  MirrorFragment,
-  PostFragment,
-  QuoteFragment,
-} from "@lens-protocol/client";
 
 export const startListener = () => {
   const client = createClient({
@@ -27,88 +21,60 @@ export const startListener = () => {
     webSocketImpl: WebSocket,
   });
 
-  client.subscribe({
-    query: newTransactionQuery,
-  }, {
-    next: (data) => {
-      handleMomokaTransaction(data as MomokaTransaction);
+  client.subscribe(
+    {
+      query: newTransactionQuery,
     },
-    // TODO: handle error + complete
-    error: (error) => {
-      console.error(error);
-    },
-    complete: () => {
-      console.log("completed");
-    },
-  });
+    {
+      next: (data) => {
+        handleMomokaTransaction(data as IMomokaTransaction);
+      },
+      // TODO: handle error + complete
+      error: (error) => {
+        console.error(error);
+      },
+      complete: () => {
+        console.log("completed");
+      },
+    }
+  );
 };
 
 export const handleMomokaTransaction = async (
-  data: MomokaTransaction
+  transaction: IMomokaTransaction
 ) => {
-  console.log("New pub:", data);
+  const data = transaction.data.newMomokaTransaction;
+  const publication = await getPublicationById(data.publication.id);
 
-  const monitoredHandles = await getMonitoredHandles();
-  if (!data || !monitoredHandles.includes(data.profile.handle.localName))
-    return;
+  if (!publication)
+    return captureException(`Publication not found: ${data.publication.id}`);
 
-  const publication = await getPublicationById(data.publicationId);
-
-  if (!publication) {
-    return new Error(`Publication not found: ${data.publicationId}`);
-  }
+  const monitoredProfileIds = await getMonitoredProfileIds();
+  if (!publication || !monitoredProfileIds.includes(publication.by.id)) return;
 
   const profile = publication.by;
-  const post =
-    publication.__typename == "Mirror" ? publication.mirrorOn : publication;
-  const type = publication.__typename;
-  const publicationUrl = getPublicationUrl(post.id);
+  const publicationUrl = getPublicationUrl(publication.id);
 
-  const targetHandle = () => {
-    if (type !== "Comment" || !(post as CommentFragment).commentOn!.by.handle)
-      return null;
+  const type = publication.__typename!;
 
-    return (post as CommentFragment).commentOn!.by.handle?.localName ?? null;
-  };
-
-  let content = MessageContent(type + "ed", publicationUrl, targetHandle());
-  const embeds = PublicationEmbed({
-    id: post.id,
-    appId: data.appId,
-    metadata: post.metadata,
-    profile:
-      type == "Mirror" ? (publication as MirrorFragment).mirrorOn.by : profile,
-  });
-
-  const quotedPost =
-    publication.__typename == "Quote"
-      ? (publication as QuoteFragment).quoteOn
-      : null;
-
-  if (quotedPost) {
-    let quotedPub = (await getPublicationById(
-      quotedPost.id
-    )) as PostFragment | null;
-
-    if (quotedPub) {
-      const quotedHandle = quotedPub.by.handle?.localName;
-      content = MessageContent("Quoted", publicationUrl, quotedHandle);
-      embeds.push(
-        ...PublicationEmbed({
-          id: quotedPub.id,
-          metadata: quotedPub.metadata,
-          profile: quotedPub.by,
-        })
-      );
-    } else
-      throw new Error(
-        `Quoted publication not found (id: ${publication.id}; quotedId: ${quotedPost.id})`
-      );
+  let posts;
+  if (publication.__typename == "Mirror") {
+    posts = [publication.mirrorOn];
+  } else if (publication.__typename == "Comment") {
+    posts = [publication.commentOn, publication];
+  } else if (publication.__typename == "Quote") {
+    posts = [publication, publication.quoteOn];
+  } else {
+    posts = [publication];
   }
+
+  let content = MessageContent(type, publicationUrl);
+
+  const embeds = posts.map((post) => PublicationEmbed(post)).flat();
 
   const payload = {
     username:
-      profile.metadata?.displayName ?? profile.handle?.localName ?? profile.id,
+      profile.metadata?.displayName ?? profile.handle?.fullHandle ?? profile.id,
     avatar_url: getPictureUrl(profile),
     content,
     embeds,
