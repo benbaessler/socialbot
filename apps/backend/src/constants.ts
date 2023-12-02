17df8a66eed0259bd91a24fee8109a17a06216a1
benbaessler <mail@benbaessler.com>
2023-12-02 15:43:22 +0000

    fix(backend): update tape logo

--- apps/backend/src/constants.ts
+++ apps/backend/src/constants.ts
@@ -32,7 +32,7 @@ export const appIcons: { [key: string]: string } = {
   orb: "https://assets-global.website-files.com/6364e65656ab107e465325d2/637adce0f72214c270ffa76e_JQtTVb7vwFnUVbc9IshTcAUewaoeO8Up_B6pqkeRFGA.jpeg",
   phaver:
     "https://play-lh.googleusercontent.com/zvGNd9l1io5wL0SVxzqIrMzA0bRoO8Dsxmam8rj_KYZSi7hxg8L7VS-_HO2HxM3k9ak",
-  tape: "https://static.tape.xyz/brand/logo.svg",
+  tape: "https://pbs.twimg.com/profile_images/1710240896611135488/720GQtzb_400x400.jpg",
   buttrfly: "https://buttrfly.app/buttrfly-icon-rounded.png",
   chainjet:
     "https://pbs.twimg.com/profile_images/1567155757761679361/k_EIJBD5_400x400.jpg",
